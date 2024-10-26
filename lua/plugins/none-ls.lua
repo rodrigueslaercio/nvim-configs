33f05552a2fa04d58ed4be8f5cdc99c95cd373e7
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2024-10-26 19:20:44 -0300

    fix timeout null-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,11 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.golines,
 				null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.erb_lint,
 			},
+      timeout_ms = 2000,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
         local config = require("nvim-treesitter.configs")
         config.setup({
           ensure_installed = {"lua", "javascript", "ruby"},
+          auto_install = true,
           highlight = { enable = true },
           indent = { enable = true },
         })
